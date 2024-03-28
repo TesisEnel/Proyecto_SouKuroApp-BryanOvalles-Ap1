3a2a40d913d0aba09cd8082ffb545d6754ba407b
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-27 21:38:44 -0400

    Creacion del modelo de Usuarios

---
+++ Shared/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models;
+
+public class Usuario
+{
+    [Key]
+    public int UsuarioId { get; set; }
+
+    [Required]
+    public string Nombre_Usuario { get; set; } = string.Empty;
+
+    [Required]
+    public string Contrasena { get; set; } = string.Empty;
+
+    [Required]
+
+    public string Rol { get; set; } = string.Empty;
+}
