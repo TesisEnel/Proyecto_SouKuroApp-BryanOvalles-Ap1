5749c47797091ffc871ac0a00b622c678003749a
BrynOvalles <brynovallesburgos@gmail.com>
2024-03-27 22:01:01 -0400

    Creando el Contexto

--- Proyecto_SouKuroApp/Proyecto_SouKuroApp/DAL/Contexto.cs
+++ Proyecto_SouKuroApp/Proyecto_SouKuroApp/DAL/Contexto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
 
 namespace Proyecto_SouKuroApp.DAL
 {
@@ -7,5 +8,17 @@ namespace Proyecto_SouKuroApp.DAL
         public Contexto(DbContextOptions<Contexto> options) : base(options)
         {
         }
+
+        public DbSet<Compra> compras { get; set; }
+        public DbSet<Producto> productos { get; set; }
+        public DbSet<Usuario> usuarios { get; set; }
+        public DbSet<Pago> pagos { get; set; }
+        public DbSet<Proveedor> proveedores { get; set; }
+        public DbSet<Venta> ventas { get; set; }
+        public DbSet<Pedido> pedidos { get; set; }
+        public DbSet<Informe_Mensual> Informes { get; set; }
+        public DbSet<Compras_Detalles> detalleCompras { get; set; }
+        public DbSet<Producto_Detalle> producto_Detalles { get; set; }
+
     }
 }
