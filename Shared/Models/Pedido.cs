f1d9201857a7e2fb2577da948fe2cb103faeb767
BrynOvalles <brynovallesburgos@gmail.com>
2024-03-27 21:44:37 -0400

    Creando las Clases

---
+++ Shared/Models/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models;
+
+public class Pedido
+{
+    [Key]
+    public int PedidoId { get; set; }
+    [Required]
+    public DateTime Fecha_Pedido { get; set; } = DateTime.Now;
+    [Required]
+    public Proveedor _Proveedor { get; set; } = new Proveedor();
+    [Required]
+    public string Direccion { get; set; } = string.Empty;
+    [Required]
+    public Compra compra { get; set; } = new Compra();
+    
+    ICollection<Pedido> Pedidos{ get; set; } = new List<Pedido>();
+
+}
---
+++ Shared/Models/Producto_Detalle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models;
+
+public class Producto_Detalle
+{
+    [Key]
+    public int Id { get; set; }
+    [ForeignKey("Producto")]
+    public int ProductoId { get; set; }
+    [Required]
+    public string Categoria { get; set; } = string.Empty;    
+
+    ICollection<Producto_Detalle> Productos_Detalles { get; set; } = new List<Producto_Detalle>();
+
+}
