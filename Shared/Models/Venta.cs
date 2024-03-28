41821f2110778d8328b4fa5f710a1f124b963c2b
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-27 21:44:52 -0400

    Creacion Del modelo de Venta

---
+++ Shared/Models/Venta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models;
+
+public class Venta
+{
+    [Key]
+    public int VentaId { get; set; }
+
+    [Required]
+    public string NombreCliente { get; set; } = string.Empty;
+
+    [Required]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    [Required]
+    public Producto producto { get; set; }
+}
