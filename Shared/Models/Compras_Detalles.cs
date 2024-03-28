2f05764803302211bebfcb71222ab6c3182a4ea7
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-27 21:30:01 -0400

    Creacion del modelo de Compras detalle

---
+++ Shared/Models/Compras_Detalles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models;
+
+    public class Compras_Detalles
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("CompraId")]
+        public int ComprasId { get; set; }
+
+        [Required] 
+        public string Nombre_Producto { get; set; } = string.Empty;
+
+        [Required]
+        public int Cantidad_Prod {  get; set; }
+ 
+    }
+
