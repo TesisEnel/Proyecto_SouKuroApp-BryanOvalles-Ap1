6d844a6d58ca39110f393a72ed4a022abbc0131b
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-04-01 12:41:48 -0400

    Agregue campos

--- Proyecto_SouKuroApp/Proyecto_SouKuroApp/Data/ApplicationUser.cs
+++ Proyecto_SouKuroApp/Proyecto_SouKuroApp/Data/ApplicationUser.cs
@@ -5,6 +5,9 @@ namespace Proyecto_SouKuroApp.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string NombreUsuario { get; set; } = string.Empty;
+
+        public DateOnly? FechaNacimiento { get; set; }
     }
 
 }
