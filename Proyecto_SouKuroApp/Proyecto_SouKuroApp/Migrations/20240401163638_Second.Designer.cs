b5c27fde1d8c1fa61f9227fbf1788e5e15606945
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-04-01 12:43:12 -0400

    hize una nueva migracion

--- Proyecto_SouKuroApp/Proyecto_SouKuroApp/Migrations/20240401163638_Second.Designer.cs
+++ Proyecto_SouKuroApp/Proyecto_SouKuroApp/Migrations/20240401163638_Second.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Proyecto_SouKuroApp.Data;
 
@@ -11,9 +12,11 @@ using Proyecto_SouKuroApp.Data;
 namespace Proyecto_SouKuroApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240401163638_Second")]
+    partial class Second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Proyecto_SouKuroApp/Proyecto_SouKuroApp/Migrations/20240401163638_Second.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto_SouKuroApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "FechaNacimiento",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NombreUsuario",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaNacimiento",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NombreUsuario",
+                table: "AspNetUsers");
+        }
+    }
+}
