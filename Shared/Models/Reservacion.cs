d494658e688b4dbeae56e0db3a1384bc50e90158
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-04-03 19:17:11 -0400

    creacion del modelo de reservacion

---
+++ Shared/Models/Reservacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+	public class Reservacion
+	{
+		[Key]
+		public int ReservacionId { get; set; }
+
+		[Required]
+		public string Nombre_Cliente { get; set; } = string.Empty;
+
+		[Required]
+		public string Cedula { get; set; } = string.Empty;
+
+
+		[Required]
+		public DateTime Fecha { get; set; }
+	}
+}
