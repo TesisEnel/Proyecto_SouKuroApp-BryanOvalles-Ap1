7c56f4c5fdc0dd65b801ce7801abd266a9040d45
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-04-03 19:16:22 -0400

    Creacion del modelo de el centro de pedidos

---
+++ Shared/Models/Centro_de_Pedidos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+	public class Centro_de_Pedidos
+	{
+		[Key]
+		public int PedidoId { get; set; }
+
+		[Required]
+		public string Nombre_Cliente { get; set; } = string.Empty;
+
+		[Required]
+		public int Numero_Entrada { get; set; }
+
+		[Required]
+		public int Num_Mesa { get; set; }
+
+		[Required]
+		public string Estado_Entrega { get; set; } = string.Empty;
+
+		[Required]
+		public string Estado_Pago { get; set; } = string.Empty;
+
+		[ForeignKey("Centro_de_Pedidos")]
+		public ICollection<Centro_Detalle> Detalle { get; set; } = new List<Centro_Detalle>();
+
+	}
+}
