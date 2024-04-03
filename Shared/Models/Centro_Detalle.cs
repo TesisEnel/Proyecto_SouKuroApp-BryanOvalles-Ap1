31f75d876870e4e423beed4b47171ae812cb0b97
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-04-03 19:16:40 -0400

    creacion del modelo del dealle del centro de pedido

---
+++ Shared/Models/Centro_Detalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+	public class Centro_Detalle
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public int PedidoId { get; set; }
+
+		[Required]
+		public string  Nombre_Produc {  get; set; } = string.Empty;
+
+		[Required]
+		public int Cantidad { get; set; }
+
+
+	}
+}
