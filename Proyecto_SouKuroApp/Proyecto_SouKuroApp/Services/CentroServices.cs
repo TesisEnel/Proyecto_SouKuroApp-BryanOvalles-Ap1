c618d1defe14ba2ede359fe56e61ae67f49ad3b4
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-04-03 20:15:18 -0400

    Creacion del Services de Centro de pedido

---
+++ Proyecto_SouKuroApp/Proyecto_SouKuroApp/Services/CentroServices.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_SouKuroApp.Data;
+using Shared.Models;
+using System.Linq.Expressions;
+
+namespace Proyecto_SouKuroApp.Services
+{
+    public class CentroServices
+    {
+        private readonly ApplicationDbContext _contexto;
+        public CentroServices(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+        public async Task<bool> Existe(int PedidoId)
+        {
+            return await _contexto.centro_de_Pedidos.AnyAsync(c => c.PedidoId == PedidoId);
+        }
+        public async Task<Centro_de_Pedidos?> GetCentroPed(int id)
+        {
+            return await _contexto.centro_de_Pedidos.Include(c => c.Detalle).FirstOrDefaultAsync(c => c.PedidoId == id);
+        }
+        public async Task<bool> Insertar(Centro_de_Pedidos pedido)
+        {
+            _contexto.centro_de_Pedidos.Add(pedido);
+            int filasAfectadas = await _contexto.SaveChangesAsync();
+            return filasAfectadas > 0;
+        }
+        public async Task<bool> Modificar(Centro_de_Pedidos pedido)
+        {
+            var c = await _contexto.centro_de_Pedidos.FindAsync(pedido.PedidoId);
+            _contexto.Entry(c!).State = EntityState.Detached;
+            _contexto.Entry(pedido).State = EntityState.Modified;
+            return await _contexto.SaveChangesAsync() > 0;
+        }
+        public async Task<bool> Guardar(Centro_de_Pedidos pedido)
+        {
+            if (!await Existe(pedido.PedidoId))
+                return await Insertar(pedido);
+            else
+                return await Modificar(pedido);
+        }
+        public async Task<bool> Eliminar(Centro_de_Pedidos pedido)
+        {
+            var c = await _contexto.centro_de_Pedidos.FindAsync(pedido.PedidoId);
+            _contexto.Entry(c!).State = EntityState.Detached;
+            _contexto.Entry(pedido).State = EntityState.Deleted;
+            return await _contexto.SaveChangesAsync() > 0;
+        }
+        public async Task<Centro_de_Pedidos?> Buscar(int PedidoId)
+        {
+            return await _contexto.centro_de_Pedidos
+                .Where(c => c.PedidoId == PedidoId)
+                .AsNoTracking()
+                .SingleOrDefaultAsync();
+            //.Include(c => c.ComprasDetalle)
+        }
+        public async Task<List<Centro_de_Pedidos>> Listar(Expression<Func<Centro_de_Pedidos, bool>> Criterio)
+        {
+            return await _contexto.centro_de_Pedidos
+                    .Where(Criterio)
+                    .AsNoTracking()
+                    .ToListAsync();
+        }
+    }
+}
